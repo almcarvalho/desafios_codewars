3fd7fb8ff002f65ee27de4992c607d5f26c7d1ab
almcarvalho <antoniolucas@live.com>
2022-06-05 00:19:27 -0300

    menor

--- src/filaSupermercado/menor.js
+++ src/filaSupermercado/menor.js
@@ -1,3 +1,29 @@
 //pegando o menor x pegando a posição do menor
 let array = [1, 2, 3];
+//pega o indice do menor:
+console.log('indice do menor:');
 console.log(array.indexOf(Math.min(...array)));
+
+//pega o menor
+console.log('menor elemento:');
+console.log(Math.min(...array));
+
+
+let till = [1, 2, 3];
+
+
+for (let index = 0; index < till.length; index++) {
+    const element = till[index];
+    console.log('item: ' + element);
+}
+
+
+
+till.forEach(currentItem => {
+    console.log('item: ' + currentItem);
+});
+
+
+for (let item in till) {
+    console.log('item: ' + item);
+}
